facd8833cb6635046eb877376bce283ca83baa66
Anastasiya Klimkova <anastasiya.klimkovaa@gmail.com>
2019-08-26 01:53:37 +0300

    init

--- src/com/leverx/model/Commandant.java
+++ src/com/leverx/model/Commandant.java
@@ -7,25 +7,7 @@ public class Commandant{
     private Commandant() {
 
     }
-    public  void run(){
-
-        //go through the cycle Hostel (all floors and rooms)
-        // каждые три секунды проверка
-        Student student = new Student("fdffd", 2);
-        if(student.getStudyCourse()>=4 || !student.isCreditworthy() ||
-                student.getRemark()>=2|| student.isExpelled() || !student.isAccessAvailable()){
-            int countStud=0;
-            int roomCount=0;
-            Guard.getInstance().evictStudent(countStud);
-        }
-
-        if(student.getStudyCourse()<=4 && student.getRemark()<2 && !student.isExpelled() && !student.isAccessAvailable()){
-            Guard.getInstance().houseStudentIn(student);
-        }
-
-
-
-    }
+    
 
     public static Commandant getInstance(){
         Commandant local = commandant;
